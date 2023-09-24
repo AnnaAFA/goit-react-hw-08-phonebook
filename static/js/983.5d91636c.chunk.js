5b80498418bde32cc1d0ac9a31a4af00cde4f861
AnnaAFA <109284238+AnnaAFA@users.noreply.github.com>
2023-09-24 09:46:14 +0000

    Deploying to gh-pages from @ AnnaAFA/goit-react-hw-08-phonebook@7204c8b9b37410efdf1ffd2916ee636478358832 🚀

---
+++ static/js/983.5d91636c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,o){o.r(t);var r=o(6151),c=(o(2791),o(9126)),n=o(184);t.default=function(){return(0,n.jsx)(r.SK,{children:(0,n.jsxs)(r.Dx,{children:["Welcome to PhoneBook ",(0,n.jsx)(c.UZE,{})]})})}}}]);
+//# sourceMappingURL=983.5d91636c.chunk.js.map
